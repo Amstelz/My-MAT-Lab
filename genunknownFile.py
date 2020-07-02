adfa21594877f71a8a6028d790ed5980dd70c167
SirapopPCC <34332956+SirapopPCC@users.noreply.github.com>
2020-07-02 18:23:56 +0700

    Add files via upload

--- genunknownFile.py
+++ genunknownFile.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
 from PIL import Image
 import numpy as np
 import cv2
@@ -23,29 +21,16 @@ def createFileList(myDir, format='.png'):
     return fileList
 
 def main():
-    kbdinput = 'x'
+    kbdinput = input('Select File : ')
     cntimagetillnow = 0
-    while kbdinput != 'end':
-        print("type your test image name:")
-        kbdinput = str(input())
-        if kbdinput == 'end':
-            break
-        inputImg = cv2.imread("Docs/{}".format(kbdinput))
-        binimg = pm.preprocess(inputImg)
-
-        dest = 'trainingImgs'
-        pm.horizontal_cut(binimg,dest)
-        cntimagetillnow = pm.vertical_cutTraining(dest,cntimagetillnow)
-        print('{}'.format(cntimagetillnow),' images now loaded.')
-    listname = []
-    with open('CsvData/TrainBook.csv', mode='r', encoding='utf-8-sig') as outfile:
-        reader = csv.reader(outfile)
-        for rows in reader:
-            listname.append(rows[0])
-    #print(listname)
-    myFileList = createFileList('trainingImgs/verticalcutoutput')
+    inputImg = cv2.imread("Docs/{}".format(kbdinput))
+    binimg = pm.preprocess(inputImg)
+    dest = 'testingImgs'
+    pm.horizontal_cut(binimg,dest)
+    cntimagetillnow = pm.vertical_cutTraining(dest,cntimagetillnow)
+    print('{}'.format(cntimagetillnow),' images now loaded.')
+    myFileList = createFileList('testingImgs/verticalcutoutput')
     res = []
-    index = 0 
     for file in myFileList:
         print(file)
         img_file = Image.open(file)
@@ -65,19 +50,14 @@ def main():
         value = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
         #value = np.select([value <= 127, value>127], [np.zeros_like(value), np.ones_like(value)])
         value = np.where(value > 127 , 1, 0)
-        name = listname[index]
         value = value.flatten()
-        last = np.array([name])
         value = np.concatenate((value, last))
         res.append(value)
         print(value)
-        index +=1
-    with open("CsvData/traindata.csv", 'w' , newline='') as f:
+    with open("CsvData/unknowndata.csv", 'w' , newline='') as f:
         writer = csv.writer(f)
         writer.writerows(res)
 
 
 if __name__ == "__main__":
     main()
-
-
